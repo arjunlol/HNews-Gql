3b50a577cfcff4dd6529c6893fec7bfc55f26c71
Arjun Lall <lallarjun@gmail.com>
2018-02-03 17:17:07 -0500

    mutation

---
+++ app/graphql/resolvers/create_link.rb
@@ -0,0 +1,19 @@
+class Resolvers::CreateLink < GraphQL::Function
+  #arguments passed as args
+  argument :description, !types.String
+  argument :url, !types.String
+
+  #return type from mutation
+  type Types::LinkType
+
+  #mutation method
+  #_obj - is parent object (in this case nil)
+  #args - arguments passed
+  #_ctx - the graphql context
+  def call(_obj, args, _ctx)
+    Link.create!(
+      description: args[:description],
+      url: args[:url],
+    )
+  end
+end
